5595454444cb29564fa735a0c20ff63b6d295c01
BuildTools <unconfigured@null.spigotmc.org>
2021-06-22 19:49:18 +0400

    added particle version support

---
+++ src/main/java/me/mherzaqaryan/sponge/particle/ParticleSupport.java
@@ -0,0 +1,10 @@
+package me.mherzaqaryan.sponge.particle;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+public interface ParticleSupport {
+
+    void play(Location location);
+
+}
